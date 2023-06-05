c975de20ec0402dd3978c006f12e37f804efd9a4
Alexandra <alexandrasolovjova@mail.ru>
2023-06-05 23:48:49 +0300

    filled class Channel. added temporary way to get API key

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,22 @@
+import os
+import json
+import get_key
+from googleapiclient.discovery import build
+
+
+#не получается забрать из переменной окружения
+#временно беру ключ из функции, возвращающую ключ в виде строки
+api_key = get_key.get_key()
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
     """Класс для ютуб-канала"""
-
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
