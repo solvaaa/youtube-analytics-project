d56373aa6d8d47aa3883c135b52409c5075d06e0
Alexandra <alexandrasolovjova@mail.ru>
2023-06-06 00:06:04 +0300

    fixed getting api-key from env variables

--- src/channel.py
+++ src/channel.py
@@ -4,18 +4,15 @@ import get_key
 from googleapiclient.discovery import build
 
 
-#не получается забрать из переменной окружения
-#временно беру ключ из функции, возвращающую ключ в виде строки
-api_key = get_key.get_key()
-youtube = build('youtube', 'v3', developerKey=api_key)
-
-
 class Channel:
     """Класс для ютуб-канала"""
+    api_key = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        self.channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
