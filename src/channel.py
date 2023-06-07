fbab038f213879858f7b710b697616aaee80f50e
Alexandra <alexandrasolovjova@mail.ru>
2023-06-07 23:27:23 +0300

    changed attributes to @property

--- src/channel.py
+++ src/channel.py
@@ -11,15 +11,38 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
         self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-        self.title = self.channel['items'][0]['snippet']['title']
-        self.description = self.channel['items'][0]['snippet']['description']
-        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
-        self.video_count = int(self.channel['items'][0]['statistics']['videoCount'])
-        self.view_count = int(self.channel['items'][0]['statistics']['viewCount'])
-        self.url = 'https://www.youtube.com/channel/'
-        self.url += channel_id
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+    @property
+    def title(self):
+        return self.channel['items'][0]['snippet']['title']
+
+    @property
+    def description(self):
+        return self.channel['items'][0]['snippet']['description']
+
+    @property
+    def subscriber_count(self):
+        return self.channel['items'][0]['statistics']['subscriberCount']
+
+    @property
+    def video_count(self):
+        return int(self.channel['items'][0]['statistics']['videoCount'])
+
+    @property
+    def view_count(self):
+        return int(self.channel['items'][0]['statistics']['viewCount'])
+
+    @property
+    def url(self):
+        url_ = 'https://www.youtube.com/channel/'
+        return url_ + self.channel_id
+
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
