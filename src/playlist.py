24384045ca376d3179e47f76f7a155f4bcbb59b7
Alexandra <alexandrasolovjova@mail.ru>
2023-06-21 22:47:23 +0300

    added total_duration, show_best_video methods

--- src/playlist.py
+++ src/playlist.py
@@ -1,7 +1,9 @@
+import datetime
 import os
-import json
+import isodate
 from googleapiclient.discovery import build
 from pprint import pprint
+import datetime
 
 class PlayList:
     """ Класс для плейлиста """
@@ -14,5 +16,31 @@ class PlayList:
                                                part='contentDetails,snippet',
                                                maxResults=50,
                                                ).execute()
+
+        video_ids = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
+        self.video_response = self.youtube.videos().list(part='contentDetails,statistics',
+                                               id=','.join(video_ids)
+                                               ).execute()
         self.url = 'https://www.youtube.com/playlist?list=' + playlist_id
         self.title = self.playlist_videos['items'][0]['snippet']['title'].split('.')[0]
+
+    @property
+    def total_duration(self):
+        total_duration = datetime.timedelta(0)
+        for video in self.video_response['items']:
+            iso_8601_duration = video['contentDetails']['duration']
+            duration = isodate.parse_duration(iso_8601_duration)
+            total_duration += duration
+        return total_duration
+
+    def show_best_video(self):
+        max_likeCount = -1
+        max_likeCount_id = 0
+        for video in self.video_response['items']:
+            current_likes = int(video['statistics']['likeCount'])
+            if current_likes > max_likeCount:
+                max_likeCount = current_likes
+                max_likeCount_id = video['id']
+        url_max_likeCount = "https://youtu.be/" + max_likeCount_id
+        return url_max_likeCount
+
